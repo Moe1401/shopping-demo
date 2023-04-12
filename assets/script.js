9c95ba55420a79a618c0bbff6f0eff287ff644fd
Mylan <moe1401@gmail.com>
2023-04-12 12:34:54 -0500

    started HTML

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+////nav bar
+ //location
+  //**assign to local storage for cart
+ //name of site
+ //cart (icon top right ish.. or link)
+ 
+//body
+ //products (look into using a bootstrap form )
+  // button that adds product to cart.
+  //description of product
+  //**two rows of cards. 4-6 items (more if needed, not using a form)
+//**store items in storage
+
