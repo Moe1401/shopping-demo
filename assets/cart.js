73ecab19ff1f960e089777ecc6ec61ec2073a90d
jglarner3 <126602666+jglarner3@users.noreply.github.com>
2023-04-16 13:17:39 -0500

    cart modifications and for loop

--- assets/cart.js
+++ assets/cart.js
@@ -10,9 +10,10 @@
 var checkoutCloseEl;
 var checkoutBtnEl = document.querySelector('.checkout-btn');
 var homepageBtnEl;
-
+var checkoutCloseEl = document.querySelector('#card-form');
+var cartContainer = document.querySelector("#cart-rows")
 var productsExtractedFromLocalStorage = localStorage.getItem("cart-products");
-
+    console.log(productsExtractedFromLocalStorage)
 
 
 
@@ -21,7 +22,21 @@ function generateCart() {
 
     for (var x = 0; x < productsExtractedFromLocalStorage.length; x++) {
         var productInCart = productsExtractedFromLocalStorage[x];
-        
+        var productCart= document.createElement("tr")
+        var productImage= document.createElement("td")
+        var icon= document.createElement("img")
+        var productName= document.createElement("td")
+        var productPrice= document.createElement("td")
+        var productBtn= document.createElement("td")
+        var remove= document.createElement("button")
+
+        productCart.setAttribute("id", "product-row")
+        productImage.setAttribute("id", "product-image")
+        icon.setAttribute ("src", "https://cdn.shopify.com/s/files/1/2382/0223/products/42127-1527888046_700x700.jpg?v=1571614304")
+        productName.setAttribute("id", "product-name")
+        productPrice.setAttribute("id", "product-price")
+        productBtn.setAttribute("id", "product-Btn")
+        remove.setAttribute("id", "remove")
     }
 }
 
@@ -35,15 +50,16 @@ addEventListener("click", function(event) {
 
 
 
-//event listener for when the checkout/finalize purchase button is clicked
-//Pulls up a form for shipping info???
-checkoutBtnEl.addEventListener("click", function(event) {
 
-})
+ //TODO ** when we close form, get rid of values entered
+ //event listener to close the checkout form
+ checkoutCloseEl.addEventListener('click', function() {
+  document.getElementById('card-form').style.display = "none";
+});
 
+// event listener to pull up form when clicking checkout button
+checkoutBtnEl.addEventListener('click', function() {
+document.getElementById("card-form").style.display = "block";
+ 
+});
 
-
-//homepage button sends the user to the index.html page
-homepageBtnEl.addEventListener("click", function(){
-    // make href = index.html
-})
